efa7002f2eac38dc76e8d2f3aa07cedb119f7bdb
Amindi Perera <59931638+Amindi99@users.noreply.github.com>
2021-07-29 14:07:10 +0530

    Add files via upload
    done

---
+++ im/about-us.php
@@ -0,0 +1,131 @@
+<!DOCTYPE php>
+<php>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>About Us</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+    <link rel="stylesheet" href="/assets/css/styles.min.css">
+
+    <style>
+
+.card{
+    height: 200px; 
+    background-color: #e6e6ff; 
+    border-color: #002649;
+}
+
+.row{
+    width: 100%;
+    margin-right: -28px; 
+    margin-left: 12px;  
+    height: 330px;
+}
+
+.card-title{
+    color: #002649;
+}
+
+body{
+    background-image: url(assets/img/avatars/about-background.jpg);
+}
+
+</style>
+
+</head>
+
+<body>
+
+    
+<?php
+		include 'header.php';
+	?>
+    
+        <section class="clean-block about-us">
+            <div class="container">
+                <div class="block-heading">
+                  
+                <!--  Start the vision mission -->
+                <br><br><br>
+                    <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Vision</h4>
+                            <p>Creating a competent Sri Lankan citizenry enriched with knowledge, skills and morals enable
+                                to achieve the demands of 21st century leading for a prosperous nation through sustainable development.</p>
+                            </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Misssion</h4>
+                            <p>Implementation of Education policies and reforms through novel educational and technological 
+                                approaches to drive the human resources mandated by national developmental principals.</p>
+                            </div>
+                    </div>
+                </div>
+                </div>
+            <!-- end of the vision mission -->
+
+
+        </section>
+        
+    </main>
+    <!-- Start: Footer Dark -->
+    <footer class="page-footer dark">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <h5>Get started</h5>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Sign up</a></li>
+                        <li><a href="#">Downloads</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <h5>About us</h5>
+                    <ul>
+                        <li><a href="#">Company Information</a></li>
+                        <li><a href="#">Contact us</a></li>
+                        <li><a href="#">Reviews</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <h5>Support</h5>
+                    <ul>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Help desk</a></li>
+                        <li><a href="#">Forums</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <h5>Legal</h5>
+                    <ul>
+                        <li><a href="#">Terms of Service</a></li>
+                        <li><a href="#">Terms of Use</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <p>© 2021 Copyright Text</p>
+        </div>
+    </footer>
+    
+    <!-- End: Footer Dark -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+    <script src="assets/js/script.min.js?h=037c4117326bceadaedc2681de8afadc"></script>
+    
+</body>
+
+</php>
